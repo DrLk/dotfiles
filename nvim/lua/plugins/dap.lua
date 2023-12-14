f63a496b068ae6bbab82ddc72c5dc1105e507210
Andrew Ermakovich <andrew@resilio.com>
2023-12-14 19:29:47 +0100

    NeoVim lua config

---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,92 @@
+local dap = require("dap")
+
+vim.keymap.set("n", "<F5>", function()
+	dap.continue()
+end)
+
+vim.keymap.set("n", "<F10>", function()
+	dap.step_over()
+end)
+vim.keymap.set("n", "<F11>", function()
+	dap.step_into()
+end)
+vim.keymap.set("n", "<F12>", function()
+	dap.step_out()
+end)
+vim.keymap.set("n", "<Leader>b", function()
+	dap.toggle_breakpoint()
+end)
+vim.keymap.set("n", "<Leader>B", function()
+	dap.set_breakpoint()
+end)
+vim.keymap.set("n", "<Leader>lp", function()
+	dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+end)
+vim.keymap.set("n", "<Leader>dr", function()
+	require("dap").repl.open()
+end)
+vim.keymap.set("n", "<Leader>dl", function()
+	require("dap").run_last()
+end)
+vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+	require("dap.ui.widgets").hover()
+end)
+vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+	require("dap.ui.widgets").preview()
+end)
+vim.keymap.set("n", "<Leader>df", function()
+	local widgets = require("dap.ui.widgets")
+	widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set("n", "<Leader>ds", function()
+	local widgets = require("dap.ui.widgets")
+	widgets.centered_float(widgets.scopes)
+end)
+
+local mason_registry = require("mason-registry")
+local codelldb_root = mason_registry.get_package("codelldb"):get_install_path() .. "/extension/"
+local codelldb_path = codelldb_root .. "adapter/codelldb"
+local liblldb_path = codelldb_root .. "lldb/lib/liblldb.so"
+dap.adapters.lldb = {
+	type = "server",
+	port = "${port}",
+	host = "127.0.0.1",
+	executable = {
+		command = codelldb_path,
+		args = { "--liblldb", liblldb_path, "--port", "${port}" },
+	},
+}
+
+dap.configurations.cpp = {
+	{
+		name = "lldb 1",
+		type = "lldb",
+		request = "launch",
+		program = "/home/ermkaovich/projects/MultiThreaded-Socket/SimpleNetworkProtocol//build/SimpleNetworkProtocol",
+		stopOnEntry = false,
+		args = {},
+	},
+	{
+		name = "lldb 2",
+		type = "lldb",
+		request = "launch",
+		program = "/home/ermkaovich/projects/MultiThreaded-Socket/SimpleNetworkProtocol//build/SimpleNetworkProtocol",
+		stopOnEntry = false,
+		args = {},
+	},
+}
+
+require("dapui").setup()
+
+local dapui = require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dapui.close()
+end
+
+-- require("dap.ext.vscode").load_launchjs(".nvim/launch.json", { lldb = { "c", "cpp", "rust" } })
