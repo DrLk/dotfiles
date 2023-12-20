659236f70b8ef2390efd8586a73f770741b8b9c1
Ermakovich Andrew <ermakovich.andrew@gmail.com>
2023-12-20 22:37:39 +0100

    Telescope normal mode

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,18 +1,29 @@
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>f.", builtin.resume, { desc = "Resume" })
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
+vim.keymap.set('n', '<leader>ff', function()
+    builtin.find_files({ initial_mode = "insert" })
+end, { noremap = true, silent = true, desc = "Find files" })
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<Tab>', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fb', function()
+    builtin.buffers({ initial_mode = "normal" })
+end, { noremap = true, silent = true, desc = "Buffers" })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
-vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
+
+vim.keymap.set('n', '<leader>gs', function()
+    builtin.git_status({ initial_mode = "normal" })
+end, { noremap = true, silent = true, desc = "Git status" })
+
 vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, {})
-vim.keymap.set('n', 'gr', builtin.lsp_references,
-    { noremap = true, silent = true })
-vim.keymap.set('n', 'gd', builtin.lsp_definitions,
-    { noremap = true, silent = true })
+
+vim.keymap.set('n', 'gr', function()
+    builtin.lsp_references({ initial_mode = "normal" })
+end, { noremap = true, silent = true, desc = "References" })
+
+vim.keymap.set('n', 'gd', function()
+    builtin.lsp_definitions({ initial_mode = "normal" })
+end, { noremap = true, silent = true, desc = "Definitions" })
 
 require('telescope').setup {
     defaults = {
