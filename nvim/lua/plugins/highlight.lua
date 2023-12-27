b8df80481fe348a20abd884b89dc800d0520cc6d
Andrew Ermakovich <andrew@resilio.com>
2023-12-27 11:09:43 +0100

    Add highlight plugin

---
+++ nvim/lua/plugins/highlight.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader><S-h>h", "<cmd>Highlight h<CR>", { desc = "Highlight line" })
+vim.keymap.set("n", "<leader><S-h>n", "<cmd>Highlight n<CR>", { desc = "Clear all highlights" })
