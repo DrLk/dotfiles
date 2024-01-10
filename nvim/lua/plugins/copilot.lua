128f446e5e493d7440e2dfaafa7b6ebfa9efeb3e
Ermakovich Andrew <ermakovich.andrew@gmail.com>
2024-01-10 22:28:22 +0100

    Copilot map

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,5 +1,5 @@
-vim.keymap.set("i", "<Tab>", "copilot#Accept('<CR>')", {
+vim.keymap.set("i", "<C-l>", "copilot#Accept('<CR>')", {
     expr = true,
     replace_keycodes = false
 })
--- vim.g.copilot_no_tab_map = true
+vim.g.copilot_no_tab_map = true
