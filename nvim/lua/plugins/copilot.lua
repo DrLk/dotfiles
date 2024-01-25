9ea7266eb17eaa627d5954e807ba375201a91536
Andrew Ermakovich <andrew@resilio.com>
2024-01-25 14:01:27 +0100

    Enable copilot only when lsp works

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -3,3 +3,11 @@ vim.keymap.set("i", "<C-l>", "copilot#Accept('<CR>')", {
     replace_keycodes = false
 })
 vim.g.copilot_no_tab_map = true
+vim.cmd("Copilot disable")
+
+vim.api.nvim_create_autocmd({ "LspAttach" }, {
+    pattern = "*",
+    callback = function(args)
+        vim.cmd("Copilot enable")
+    end
+})
