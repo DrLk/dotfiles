71ee41b5d18dde9be9418bf2e70b4cef0a2f0cda
Ermakovich Andrew <ermakovich.andrew@gmail.com>
2024-04-29 19:53:59 +0200

    Use github algoritm histogram

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -106,7 +106,7 @@ gitsigns.setup {
     linehl                            = true,  -- Toggle with `:Gitsigns toggle_linehl`
     word_diff                         = false, -- Toggle with `:Gitsigns toggle_word_diff`
     diff_opts                         = {
-        algorithm = "patience",
+        algorithm = "histogram",
         ignore_whitespace_change = true,
     },
     watch_gitdir                      = {
