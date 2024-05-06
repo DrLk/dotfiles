8a3f386adeb2cb48a4073ccf90bde92919093bd8
Andrew Ermakovich <andrew@resilio.com>
2024-05-06 12:16:09 +0200

    Setup animate nvim plugin

--- nvim/lua/plugins/animate.lua
+++ nvim/lua/plugins/animate.lua
@@ -1,7 +1,17 @@
-require('mini.animate').setup({
+local animate = require('mini.animate')
+animate.setup({
+    cursor = {
+        -- Animate for 200 milliseconds with linear easing
+        timing = animate.gen_timing.linear({ duration = 200, unit = 'total' }),
+
+        -- Animate with shortest line for any cursor move
+        path = animate.gen_path.line({
+            predicate = function() return true end,
+        }),
+    },
     -- Vertical scroll
-  scroll = {
-    -- Whether to enable this animation
-    enable = false,
-  }
+    scroll = {
+        -- Whether to enable this animation
+        enable = false,
+    }
 })
