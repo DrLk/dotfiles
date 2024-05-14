182a6545410f3467249b77841062c557316bc7a8
Andrew Ermakovich <andrew@resilio.com>
2024-05-14 13:40:59 +0200

    Fix hyprlock

--- hypr/scripts/LockScreen.sh
+++ hypr/scripts/LockScreen.sh
@@ -2,9 +2,9 @@
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 
 
-if [ $XDG_CURRENT_DESKTOP == "sway" ]; then
-    swaylock --daemonize --color "$selection-color" --inside-color "$selection-color" --inside-clear-color "$text-color" --ring-color "$color2" --ring-clear-color "$color11" --ring-ver-color "$color13" --show-failed-attempts --fade-in 0.2 --grace 2 --effect-vignette 0.5:0.5 --effect-blur 7x5 --ignore-empty-password --screenshots --clock
-else
+if [ -x "$(command -v hyprlock)" ]; then
     # For Hyprlock
     hyprlock -q
+else
+    swaylock --daemonize --color "$selection-color" --inside-color "$selection-color" --inside-clear-color "$text-color" --ring-color "$color2" --ring-clear-color "$color11" --ring-ver-color "$color13" --show-failed-attempts --fade-in 0.2 --grace 2 --effect-vignette 0.5:0.5 --effect-blur 7x5 --ignore-empty-password --screenshots --clock
 fi
