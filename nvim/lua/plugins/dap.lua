fa236771ae7a2df61ab5127711539631c1e4c2a3
Andrew Ermakovich <andrew@resilio.com>
2024-05-15 13:03:05 +0200

    Remove type from dap view

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -257,7 +257,7 @@ require("dapui").setup({
         },
     },
     render = {
-        max_type_length = nil, -- Can be integer or nil.
+        max_type_length = 0,  -- Can be integer or nil.
         max_value_lines = 100, -- Can be integer or nil.
         indent = 1,
     },
