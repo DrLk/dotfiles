c2fc812921d4323e602f30895796b4d78e1594e6
Ermakovich Andrew <ermakovich.andrew@gmail.com>
2024-05-18 14:47:56 +0200

    Fix silent binding

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -1,18 +1,18 @@
 vim.g.mapleader = " "
 
--- NeoTree
-vim.keymap.set("n", "<leader>e", ":Neotree float reveal<CR>")
--- vim.keymap.set("n", "<leader>E", ":Neotree left reveal<CR>")
-vim.keymap.set("n", "<leader>o", ":Neotree float git_status<CR>")
-
-
 local opt = { noremap = true, silent = true }
+-- NeoTree
+vim.keymap.set("n", "<leader>e", ":Neotree float reveal<CR>", opt)
+-- vim.keymap.set("n", "<leader>E", ":Neotree left reveal<CR>")
+vim.keymap.set("n", "<leader>o", ":Neotree float git_status<CR>", opt)
+
+
 -- Navigation
 vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", opt)
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", opt)
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", opt)
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opt)
-vim.keymap.set("n", "<leader>/", ":CommentToggle<CR>")
+vim.keymap.set("n", "<leader>/", ":CommentToggle<CR>", opt)
 
 -- Splits
 vim.keymap.set("n", "|", ":vsplit<CR>")
@@ -20,12 +20,12 @@ vim.keymap.set("n", "\\", ":split<CR>")
 
 -- Other
 vim.keymap.set("i", "jj", "<Esc>")
-vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", opt)
 
 -- Terminal
-vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>")
-vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>")
-vim.keymap.set("n", "<leader>tv", ":ToggleTerm direction=vertical size=40<CR>")
+vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>", opt)
+vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>", opt)
+vim.keymap.set("n", "<leader>tv", ":ToggleTerm direction=vertical size=40<CR>", opt)
 
 -- Clipboard functionality (paste from system)
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
@@ -44,8 +44,7 @@ local function quickfix()
 		apply = true,
 	})
 end
-local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<leader>q", quickfix, opts)
+vim.keymap.set("n", "<leader>q", quickfix, opt)
 
 -- Quit
-vim.keymap.set("n", "<leader>Q", ":qa!<CR>", opts)
+vim.keymap.set("n", "<leader>Q", ":qa!<CR>", opt)
